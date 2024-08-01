b0a8f4c7f317a12c6851c9c10a7dc1970a067114
PersysKS <kslee@persys-tech.com>
2024-08-01 15:54:18 +0800

    End of section 7

--- API/DTOs/RegisterDto.cs
+++ API/DTOs/RegisterDto.cs
@@ -6,7 +6,8 @@ public class RegisterDto
 {
     [Required(ErrorMessage = "Username is required.")]
     [MinLength(3, ErrorMessage = "Username must be at least 3 characters long.")]
-    public  string Username { get; set; }
+    public  string Username { get; set; } = string.Empty;
+
     [Required(ErrorMessage = "Passowrd is required.")]
-    public  string Password { get; set; }
+    public  string Password { get; set; } = String.Empty;
 }
